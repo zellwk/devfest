8425c95f5f8c1ec03201a13e5ba9c6455065ee84
Zell Liew <zellwk@gmail.com>
2015-07-09 17:27:12 +0800

    add jquery form
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,9 @@ require('./svg');
 require('./zell-scrollspy');
 require('./nav-scroll');
 require('./canvas');
+require('./jqueryform');
 
+// Header 
 $(window).load(function() {
   var $el = $('.jsFixedHeader');
   var $clone = $('.jsFixedHeader').clone(true, true).removeClass('jsFixedHeader').addClass('jsFixedHeaderClone');
@@ -51,3 +53,48 @@ $(window).load(function() {
     }
   });
 })
+
+$(document).ready(function() {
+  
+  $("#subscribeForm").ajaxForm({
+    url: "http://2014.cssconf.asia/addsubscriber.php",
+    dataType: "html",
+    beforeSubmit: function() {
+      $("#subscribeForm").removeClass("failure success").addClass("load");
+      $("#subscribeForm .msg").removeClass("play");
+    },
+    success: function(r) {
+      console.log(r);
+      if (r.substr(0, 6) != "Thanks") {
+        $("#subscribeForm").removeClass("load").addClass("failure");
+        $("#subscribeForm .msg span").text(r.substr(0, r.indexOf('<br/>')));
+        $("#subscribeForm .msg").addClass("play");
+        setTimeout(function() {
+          $("#subscribeForm").removeClass("failure");
+          $("#subscribeForm input[name='email']").focus();
+        }, 5000);
+      } else {
+        $("#subscribeForm").removeClass("load").addClass("success");
+        $("#subscribeForm .msg span").text("Thanks. We'll keep you updated!");
+        $("#subscribeForm .msg").addClass("play");
+        setTimeout(function() {
+          $("#subscribeForm input[name='email']").val("");
+          $("#subscribeForm").removeClass("success active");
+          $("#subscribeForm .msg").removeClass("play");
+        }, 3000);
+        setTimeout(function() {
+          window.location = "http://facebook.com/cssconfasia";
+        }, 3200);
+      }
+    },
+    error: function(r, s) {
+      console.log(r);
+      $("#subscribeForm").removeClass("load").addClass("failure");
+      $("#subscribeForm .msg span").text("Something went utterly wrong...");
+      setTimeout(function() {
+        $("#subscribeForm").removeClass("failure");
+      }, 4000);
+    }
+  });
+
+});

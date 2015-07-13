82f8b10471902d8ff2b1071def5c913ef75b23f2
Zell Liew <zellwk@gmail.com>
2015-07-13 21:44:50 +0800

    touch up + add social
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- app/js/main.js
+++ app/js/main.js
@@ -11,90 +11,86 @@ require('./canvas');
 require('./jqueryform');
 
 // Header 
-$(window).load(function() {
-  var $el = $('.jsFixedHeader');
-  var $clone = $('.jsFixedHeader').clone(true, true).removeClass('jsFixedHeader').addClass('jsFixedHeaderClone');
+// $(window).load(function() {
+//   var $el = $('.jsFixedHeader');
+//   var $clone = $('.jsFixedHeader').clone(true, true).removeClass('jsFixedHeader').addClass('jsFixedHeaderClone');
 
-  var activationPos = $el.position().top;
+//   var activationPos = $el.position().top;
 
-  $clone.css({
-    display: 'none',
-    position: 'absolute', 
-    top: '0',
-    left: '0',
-    right: '0',
-    zIndex: '9999'
-  });
+//   $clone.css({
+//     display: 'none',
+//     position: 'absolute', 
+//     top: '0',
+//     left: '0',
+//     right: '0',
+//     zIndex: '9999'
+//   });
 
-  $el.after($clone);
+//   $el.after($clone);
 
-  function activateFixed() {
-    $clone.css({
-      'display': 'block',
-      'position': 'fixed'
-    })
-  }
+//   function activateFixed() {
+//     $clone.css({
+//       'display': 'block',
+//       'position': 'fixed'
+//     })
+//   }
 
-  function deactivateFixed() {
-    $clone.css({
-      'display': 'none',
-      'position': 'absolute',
-    })
-  }
+//   function deactivateFixed() {
+//     $clone.css({
+//       'display': 'none',
+//       'position': 'absolute',
+//     })
+//   }
 
-  $('.c-canvas__on-canvas').scroll(function(event) {
-    var $container = $('.c-canvas__on-canvas');
-    if ($container.scrollTop() > activationPos) {
-      console.log('activated');
-      activateFixed();
-    } else {
-      console.log('deactivated');
-      deactivateFixed();
-    }
-  });
-})
+//   $('.c-canvas__on-canvas').scroll(function(event) {
+//     var $container = $('.c-canvas__on-canvas');
+//     if ($container.scrollTop() > activationPos) {
+//       console.log('activated');
+//       activateFixed();
+//     } else {
+//       console.log('deactivated');
+//       deactivateFixed();
+//     }
+//   });
+// })
 
 $(document).ready(function() {
-  
+  var $form = $("#subscribeForm");
+  var  $text = $('#subscribeForm .msg').find('span');
+  var $spinner = $form.find('.spinner');
+  var $social = $('.social');
+
   $("#subscribeForm").ajaxForm({
     url: "http://2014.cssconf.asia/addsubscriber.php",
     dataType: "html",
     beforeSubmit: function() {
-      $("#subscribeForm").removeClass("failure success").addClass("load");
-      $("#subscribeForm .msg").removeClass("play");
+      // Resets text before submitting 
+      $text.text("");
+      // Starts spinner
+      $spinner.show().addClass('play');
+      $social.hide();
     },
     success: function(r) {
-      console.log(r);
       if (r.substr(0, 6) != "Thanks") {
-        $("#subscribeForm").removeClass("load").addClass("failure");
-        $("#subscribeForm .msg span").text(r.substr(0, r.indexOf('<br/>')));
-        $("#subscribeForm .msg").addClass("play");
-        setTimeout(function() {
-          $("#subscribeForm").removeClass("failure");
-          $("#subscribeForm input[name='email']").focus();
-        }, 5000);
+        console.log("No Thanks");
+        $text.text(r.substr(0, r.indexOf('<br/>')));
       } else {
-        $("#subscribeForm").removeClass("load").addClass("success");
-        $("#subscribeForm .msg span").text("Thanks. We'll keep you updated!");
-        $("#subscribeForm .msg").addClass("play");
-        setTimeout(function() {
-          $("#subscribeForm input[name='email']").val("");
-          $("#subscribeForm").removeClass("success active");
-          $("#subscribeForm .msg").removeClass("play");
-        }, 3000);
-        setTimeout(function() {
-          window.location = "http://facebook.com/cssconfasia";
-        }, 3200);
+        console.log("Thanks");
+        $form.find('input').val('');
+        $text.text("Thanks. We'll keep you updated!");
+
+        $form.find('.social-msg').text('You might want to check out our Facebook or Twitter page :)')
+        $social.show().addClass('form-show');
       }
     },
     error: function(r, s) {
-      console.log(r);
-      $("#subscribeForm").removeClass("load").addClass("failure");
-      $("#subscribeForm .msg span").text("Something went utterly wrong...");
-      setTimeout(function() {
-        $("#subscribeForm").removeClass("failure");
-      }, 4000);
+      // $("#subscribeForm").removeClass("load").addClass("failure");
+      $text.text("Something went utterly wrong...");
+    },
+    complete: function() {
+      console.log('complete');
+      $spinner.hide().removeClass('play');
+      // Stops spinner
     }
   });
-
 });

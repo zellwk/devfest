41dd3d2497fc53e2be971ac02ccf93213e8d589f
Zell Liew <zellwk@gmail.com>
2015-07-16 17:56:34 +0800

    Update dev branch with header as well
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- app/js/main.js
+++ app/js/main.js
@@ -11,91 +11,91 @@ require('./canvas');
 require('./jqueryform');
 
 // Header 
-// $(window).load(function() {
-//   var $el = $('.jsFixedHeader');
-//   var $clone = $('.jsFixedHeader').clone(true, true).removeClass('jsFixedHeader').addClass('jsFixedHeaderClone');
+$(window).load(function() {
+  var $el = $('.jsFixedHeader');
+  var $clone = $('.jsFixedHeader').clone(true, true).removeClass('jsFixedHeader').addClass('jsFixedHeaderClone');
 
-//   var activationPos = $el.position().top;
+  var activationPos = $el.position().top;
 
-//   $clone.css({
-//     display: 'none',
-//     position: 'absolute', 
-//     top: '0',
-//     left: '0',
-//     right: '0',
-//     zIndex: '9999'
-//   });
+  $clone.css({
+    display: 'none',
+    position: 'absolute', 
+    top: '0',
+    left: '0',
+    right: '0',
+    zIndex: '9999'
+  });
 
-//   $el.after($clone);
+  $el.after($clone);
 
-//   function activateFixed() {
-//     $clone.css({
-//       'display': 'block',
-//       'position': 'fixed'
-//     })
-//   }
+  function activateFixed() {
+    $clone.css({
+      'display': 'block',
+      'position': 'fixed'
+    })
+  }
 
-//   function deactivateFixed() {
-//     $clone.css({
-//       'display': 'none',
-//       'position': 'absolute',
-//     })
-//   }
+  function deactivateFixed() {
+    $clone.css({
+      'display': 'none',
+      'position': 'absolute',
+    })
+  }
 
-//   $('.c-canvas__on-canvas').scroll(function(event) {
-//     var $container = $('.c-canvas__on-canvas');
-//     if ($container.scrollTop() > activationPos) {
-//       console.log('activated');
-//       activateFixed();
-//     } else {
-//       console.log('deactivated');
-//       deactivateFixed();
-//     }
-//   });
-// })
-
-$(document).ready(function() {
-  var $form = $("#subscribeForm");
-  var  $text = $('#subscribeForm .msg').find('span');
-  var $spinner = $form.find('.spinner');
-  var $social = $('.social');
-
-  $("#subscribeForm").ajaxForm({
-    url: "http://2014.cssconf.asia/addsubscriber.php",
-    dataType: "html",
-    beforeSubmit: function() {
-      // Resets text before submitting 
-      $text.text("");
-      // Starts spinner
-      $spinner.show().addClass('play');
-      $social.hide();
-    },
-    success: function(r) {
-      if (r.substr(0, 6) != "Thanks") {
-        console.log("No Thanks");
-        $text.text(r.substr(0, r.indexOf('<br/>')));
-      } else {
-        console.log("Thanks");
-        $form.find('input').val('');
-        $text.text("Thanks. We'll keep you updated!");
-
-        $form.find('.social-msg').text('Check out our facebook page now! :)')
-        // $social.show().addClass('form-show');
-        
-        setTimeout(function() {
-          window.location.href = 'http://facebook.com/devfestasia'
-        }, 2000);
-
-      }
-    },
-    error: function(r, s) {
-      // $("#subscribeForm").removeClass("load").addClass("failure");
-      $text.text("Something went utterly wrong...");
-    },
-    complete: function() {
-      console.log('complete');
-      $spinner.hide().removeClass('play');
-      // Stops spinner
+  $('.c-canvas__on-canvas').scroll(function(event) {
+    var $container = $('.c-canvas__on-canvas');
+    if ($container.scrollTop() > activationPos) {
+      console.log('activated');
+      activateFixed();
+    } else {
+      console.log('deactivated');
+      deactivateFixed();
     }
   });
-});
+})
+
+// $(document).ready(function() {
+//   var $form = $("#subscribeForm");
+//   var  $text = $('#subscribeForm .msg').find('span');
+//   var $spinner = $form.find('.spinner');
+//   var $social = $('.social');
+
+//   $("#subscribeForm").ajaxForm({
+//     url: "http://2014.cssconf.asia/addsubscriber.php",
+//     dataType: "html",
+//     beforeSubmit: function() {
+//       // Resets text before submitting 
+//       $text.text("");
+//       // Starts spinner
+//       $spinner.show().addClass('play');
+//       $social.hide();
+//     },
+//     success: function(r) {
+//       if (r.substr(0, 6) != "Thanks") {
+//         console.log("No Thanks");
+//         $text.text(r.substr(0, r.indexOf('<br/>')));
+//       } else {
+//         console.log("Thanks");
+//         $form.find('input').val('');
+//         $text.text("Thanks. We'll keep you updated!");
+
+//         $form.find('.social-msg').text('Check out our facebook page now! :)')
+//         // $social.show().addClass('form-show');
+        
+//         setTimeout(function() {
+//           window.location.href = 'http://facebook.com/devfestasia'
+//         }, 2000);
+
+//       }
+//     },
+//     error: function(r, s) {
+//       // $("#subscribeForm").removeClass("load").addClass("failure");
+//       $text.text("Something went utterly wrong...");
+//     },
+//     complete: function() {
+//       console.log('complete');
+//       $spinner.hide().removeClass('play');
+//       // Stops spinner
+//     }
+//   });
+// });

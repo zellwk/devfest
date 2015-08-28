1d17061a0351696399c6ca8e94bde17f32c99cb4
Zell Liew <zellwk@gmail.com>
2015-08-28 10:08:07 +0800

    add COC and TOC pages
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- app/js/b/bundle.js
+++ app/js/b/bundle.js
@@ -415,6 +415,7 @@ $(document).ready(function () {
 });
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+
 },{"./canvas":1,"./jqueryform":2,"./nav-scroll":4,"./svg":5,"./zell-scrollspy":6,"jquery":"jquery"}],4:[function(require,module,exports){
 'use strict';
 
@@ -1205,7 +1206,7 @@ $(window).load(function () {
 
 },{}],"jquery":[function(require,module,exports){
 /*!
- * jQuery JavaScript Library v2.1.4
+ * jQuery JavaScript Library v2.1.3
  * http://jquery.com/
  *
  * Includes Sizzle.js
@@ -1215,7 +1216,7 @@ $(window).load(function () {
  * Released under the MIT license
  * http://jquery.org/license
  *
- * Date: 2015-04-28T16:01Z
+ * Date: 2014-12-18T15:11Z
  */
 
 (function( global, factory ) {
@@ -1273,7 +1274,7 @@ var
 	// Use the correct document accordingly with window argument (sandbox)
 	document = window.document,
 
-	version = "2.1.4",
+	version = "2.1.3",
 
 	// Define a local copy of jQuery
 	jQuery = function( selector, context ) {
@@ -1737,12 +1738,7 @@ jQuery.each("Boolean Number String Function Array Date RegExp Object Error".spli
 });
 
 function isArraylike( obj ) {
-
-	// Support: iOS 8.2 (not reproducible in simulator)
-	// `in` check used to prevent JIT error (gh-2145)
-	// hasOwn isn't used here due to false negatives
-	// regarding Nodelist length in IE
-	var length = "length" in obj && obj.length,
+	var length = obj.length,
 		type = jQuery.type( obj );
 
 	if ( type === "function" || jQuery.isWindow( obj ) ) {
@@ -22654,6 +22650,8 @@ return jQuery;
 }.call(this));
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[3]);
+
+},{}]},{},[3])
+
 
 //# sourceMappingURL=bundle.js.map

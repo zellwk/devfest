94c280545d919d0965885d5adef7fee6188d407d
Thomas Gorissen <serrynaimo@me.com>
2015-09-04 13:40:55 +0800

    updated content

--- app/js/b/bundle.js
+++ app/js/b/bundle.js
@@ -437,6 +437,7 @@ $(window).load(function () {
 // });
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+
 },{"./canvas":1,"./jqueryform":2,"./nav-scroll":4,"./svg":5,"./zell-scrollspy":6,"jquery":"jquery"}],4:[function(require,module,exports){
 'use strict';
 
@@ -1242,7 +1243,7 @@ $(window).load(function () {
 
 },{}],"jquery":[function(require,module,exports){
 /*!
- * jQuery JavaScript Library v2.1.3
+ * jQuery JavaScript Library v2.1.4
  * http://jquery.com/
  *
  * Includes Sizzle.js
@@ -1252,7 +1253,7 @@ $(window).load(function () {
  * Released under the MIT license
  * http://jquery.org/license
  *
- * Date: 2014-12-18T15:11Z
+ * Date: 2015-04-28T16:01Z
  */
 
 (function( global, factory ) {
@@ -1310,7 +1311,7 @@ var
 	// Use the correct document accordingly with window argument (sandbox)
 	document = window.document,
 
-	version = "2.1.3",
+	version = "2.1.4",
 
 	// Define a local copy of jQuery
 	jQuery = function( selector, context ) {
@@ -1774,7 +1775,12 @@ jQuery.each("Boolean Number String Function Array Date RegExp Object Error".spli
 });
 
 function isArraylike( obj ) {
-	var length = obj.length,
+
+	// Support: iOS 8.2 (not reproducible in simulator)
+	// `in` check used to prevent JIT error (gh-2145)
+	// hasOwn isn't used here due to false negatives
+	// regarding Nodelist length in IE
+	var length = "length" in obj && obj.length,
 		type = jQuery.type( obj );
 
 	if ( type === "function" || jQuery.isWindow( obj ) ) {
@@ -22686,6 +22692,8 @@ return jQuery;
 }.call(this));
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[3]);
+
+},{}]},{},[3])
+
 
 //# sourceMappingURL=bundle.js.map

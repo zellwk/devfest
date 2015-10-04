57718e8b6b52cfde42b13c69f97704de79c2bbca
Zell Liew <zellwk@gmail.com>
2015-10-04 22:10:59 +0800

    add SG
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- app/js/b/bundle.js
+++ app/js/b/bundle.js
@@ -101,6 +101,7 @@ $(document).ready(function () {
 });
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+
 },{}],3:[function(require,module,exports){
 "use strict";
 
@@ -431,6 +432,7 @@ $(document).ready(function () {
 });
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+
 },{"./canvas":1,"./hash-scroll":2,"./jqueryform":3,"./simple-header":5,"./svg":6,"./zell-scrollspy":7}],5:[function(require,module,exports){
 (function (global){
 'use strict';
@@ -484,6 +486,7 @@ $(window).ready(function () {
 });
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+
 },{}],6:[function(require,module,exports){
 'use strict';
 
@@ -22706,6 +22709,8 @@ return jQuery;
 }.call(this));
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[4]);
+
+},{}]},{},[4])
+
 
 //# sourceMappingURL=bundle.js.map

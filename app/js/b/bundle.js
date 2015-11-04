f3e99bb20951a66d488beb0d53f16e67a696f659
Thomas Gorissen <mail@serrynaimo.com>
2015-11-04 22:13:58 +0800

    code retreat fix

--- app/js/b/bundle.js
+++ app/js/b/bundle.js
@@ -45,7 +45,7 @@ module.exports={
       "events": [{
         "title": "Global Day of Coderetreat",
         "image": "images/events/coderetreat.jpg",
-        "desc": "Coderetreat is a day-long, intensive practice event, focusing on the fundamentals of software development and design. By providing developers the opportunity to take part in focused practice, away from the pressures of 'getting things done', the coderetreat format has proven itself to be a highly effective means of skill improvement. Practicing the basic principles of modular and object-oriented design, developers can improve their ability to write code that minimizes the cost of change over time.<br/><br/><a href='coderetreat.org/events/global-day-of-coderetreat-2015-singapore' target='_blank'>coderetreat.org/events/global-day-of-coderetreat-2015-singapore</a>",
+        "desc": "Coderetreat is a day-long, intensive practice event, focusing on the fundamentals of software development and design. By providing developers the opportunity to take part in focused practice, away from the pressures of 'getting things done', the coderetreat format has proven itself to be a highly effective means of skill improvement. Practicing the basic principles of modular and object-oriented design, developers can improve their ability to write code that minimizes the cost of change over time.<br/><br/><a href='http://coderetreat.org/events/global-day-of-coderetreat-2015-singapore' target='_blank'>Code Retreat Singapore Website</a>",
         "startTime": "9",
         "startTimeSuffix": "am",
         "endTime": "5",
